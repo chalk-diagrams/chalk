3dd9572fbe6aa1cfced50e3b6aa303b4c45c71a5
Dan Oneață <dan.oneata@gmail.com>
2022-01-04 18:00:05 +0200

    More organization files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="pydiagrams",
+    version="0.1dev",
+    packages=[
+        "diagrams",
+    ],
+    description="Python Distribution Utilities",
+    long_description=open("README.md").read(),
+    author="Dan Oneață",
+    author_email="dan.oneata@gmail.com",
+    # url='https://www.python.org/sigs/distutils-sig/',
+)
