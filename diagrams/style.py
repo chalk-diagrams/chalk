653a42e1e7f1e01045db78814781ca5900f4c853
Sasha Rush <srush.research@gmail.com>
2022-05-06 12:35:34 -0400

    Add options to style

--- diagrams/style.py
+++ diagrams/style.py
@@ -1,32 +1,41 @@
-from typing import Any, Optional
+from typing import Any, Optional, List, Tuple
 
-from dataclasses import dataclass
+from dataclasses import astuple, dataclass, fields
+
 
 from colour import Color  # type: ignore
 
 
 PyCairoContext = Any
 
+def m(a, b):
+    return a if a is not None else b
+
+LC = Color("black")
+LW = 0.01
 
 @dataclass
 class Style:
-    LC = Color("black")
-    LW = 0.01
-
-    def __init__(
-        self,
-        line_width: Optional[float] = None,
-        line_color: Optional[Color] = None,
-        fill_color: Optional[Color] = None,
-    ):
-        self.line_width = line_width
-        self.line_color = line_color
-        self.fill_color = fill_color
+    line_width: Optional[float] = None
+    line_color: Optional[Color] = None
+    fill_color: Optional[Color] = None
+    font_size: Optional[float] = None
+    dashing: Optional[Tuple[List[float], float]] = None
 
     @classmethod
     def default(cls) -> "Style":
-        return cls(line_width=cls.LW, line_color=cls.LC)
+        return cls(line_width=LW, line_color=LC)
 
+    def merge(self, other: "Style"):
+        return Style(*(m(getattr(other, dim.name), getattr(self, dim.name))
+                       for dim in fields(self)))
+
+    def prerender(self, ctx: PyCairoContext) -> None:
+        if self.font_size is not None:
+            print("setting", self.font_size)
+            ctx.set_font_size(self.font_size)
+        
+        
     def render(self, ctx: PyCairoContext) -> None:
         if self.fill_color:
             ctx.set_source_rgb(*self.fill_color.rgb)
@@ -34,15 +43,20 @@ class Style:
 
         # set default values if they are not provided
         if self.line_color is None:
-            lc = self.LC
+            lc = LC
         else:
             lc = self.line_color
 
         if self.line_width is None:
-            lw = self.LW
+            lw = LW
         else:
             lw = self.line_width
 
         ctx.set_source_rgb(*lc.rgb)
         ctx.set_line_width(lw)
+                    
+        if self.dashing is not None:
+            ctx.set_dash(self.dashing[0], self.dashing[1])
+
+        
         ctx.stroke()
