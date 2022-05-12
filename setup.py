24f09484f5f55659a71126ba4ee21f6186af1155
Dan Oneață <dan.oneata@gmail.com>
2022-05-12 22:36:34 +0300

    MINOR Set README type in setup

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     ],
     description="A declarative drawing API",
     long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Dan Oneață",
     author_email="dan.oneata@gmail.com",
     url="https://github.com/danoneata/chalk",
